7d86ffedce5f97b70d5fcc1c46414641b52a46dd
Abdulaziz-i <ibodullaevceo@gmail.com>
2025-04-10 14:53:13 +0500

    Delete lesson-1/homework/hw.sql

--- lesson-1/homework/hw.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE Students (StudentID INT PRIMARY KEY, Name VARCHAR(100), Course VARCHAR(50));
-INSERT INTO Students (StudentID, Name, Course)
-VALUES (101,'Asu', '2');
-SELECT*FROM Students;
